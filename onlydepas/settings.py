fc8a0081d2ec5f22b09fa88ca3d8d3923408cc1c
jaime-godoy <jaimegodoy1990@gmail.com>
2024-05-17 12:45:02 -0400

    arreglando crf

--- onlydepas/settings.py
+++ onlydepas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','https://depas-igfm.onrender.com/']
+ALLOWED_HOSTS = ['*','depas-igfm.onrender.com/']
 CSRF_TRUSTED_ORIGINS = ['https://localhost:8000','https://depas-igfm.onrender.com']
 
 LOGIN_REDIRECT_URL = 'index'
