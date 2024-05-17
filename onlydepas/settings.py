4c6a4e397745aa542bd3475c4c0ef5b694cbeb20
jaime-godoy <jaimegodoy1990@gmail.com>
2024-05-17 12:05:04 -0400

    agregado CSRF_TRUSTED_ORIGINS en settings.py

--- onlydepas/settings.py
+++ onlydepas/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-# CSRF_TRUSTED_ORIGINS = [mi url]
+CSRF_TRUSTED_ORIGINS = ['https://depas-igfm.onrender.com/']
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'  # URL a la que se redirige si un usuario no está autenticado y trata de acceder a una vista protegida
